09dc44addd0716104995f47c4a4f2515e7930ddd
Himantha Hirushan <himanthad4@gmail.com>
2022-12-08 03:13:20 +0530

    add if else part to grade according to marks

--- mainP.c
+++ mainP.c
@@ -12,7 +12,28 @@ printf("Enter your marks :");
 scanf("%d",&marks);
 
 
-prntf("%s",&name);
+printf("your name is : %s\n",name );
+
+if(marks >=75){
+    printf("you got an A pass");
+}
+
+else if(marks >=65){
+    printf("you got a B pass");
+}
+
+else if(marks >=55){
+    printf("you got a C pass");
+}
+
+else if(marks >=35){
+    printf("you got a S pass");
+}
+
+else{
+    printf("you are fail");
+}
+
 
     return 0;
 }
