bf8ed4b1956ab8361c5554c273a80ada610682cb
Himantha Hirushan <himanthad4@gmail.com>
2022-12-08 03:19:20 +0530

    add some printf code lines

--- mainP.c
+++ mainP.c
@@ -16,23 +16,33 @@ printf("your name is : %s\n",name );
 
 if(marks >=75){
     printf("you got an A pass");
+      printf("good job man.keep it up");
 }
 
 else if(marks >=65){
     printf("you got a B pass");
+      printf("good job try for  A pass");
 }
 
 else if(marks >=55){
     printf("you got a C pass");
+      printf("good job try for B pas");
 }
 
 else if(marks >=35){
-    printf("you got a S pass");
+    printf("you got a S pass\n");
+    printf("you can do more than this");
 }
 
 else{
-    printf("you are fail");
+    printf("you are fail\n");
+    printf("please try again");
 }
+
+
+
+
+
 
 
     return 0;
